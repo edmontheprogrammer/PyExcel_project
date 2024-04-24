a2dd9d8424f874bb403401c480aec6566d06a2fd
Edmon Sebit <edmontheprogrammer@gmail.com>
2024-04-24 15:45:39 -0600

    Accessing spreadsheet cells

--- app.py
+++ app.py
@@ -8,7 +8,5 @@ print(wb.sheetnames)
 sheet = wb["Sheet1"]
 
 cell = sheet["a1"]
-for row in range(1, sheet.max_row + 1):
-    for column in range(1, sheet.max_column + 1):
-        cell = sheet.cell(row, column)
-        print(cell.value)
+column = sheet["a"]
+print(column)
